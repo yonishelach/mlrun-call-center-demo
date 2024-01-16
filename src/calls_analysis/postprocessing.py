7ee4d61abd8f761e27de1a2e190424dc1b359a70
yonishelach <yonatanshelach@gmail.com>
2024-01-16 13:24:09 +0200

    fix postprocessing answers

--- src/calls_analysis/postprocessing.py
+++ src/calls_analysis/postprocessing.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 
 def postprocess_answers(answers: pd.DataFrame):
-    answers["concern_addressed"] = answers["concern_addressed"].apply(
-        lambda x: "yes" in x
-    )
+    for column in ["concern_addressed", "upsale_attempted", "upsale_success"]:
+        answers[column] = answers[column].apply(
+            lambda x: "yes" in x.casefold()
+        )
 
     return answers
